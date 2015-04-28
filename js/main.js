a79e99971452dba2173e52f3d44abd9576a16876
Kiki Jiang <itsqiqi@hotmail.com>
2015-04-27 22:41:35 -0700

    error test

--- js/main.js
+++ js/main.js
@@ -52,6 +52,34 @@ function showResults() {
   };
 }
 
+// function getCurrentStation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       console.log(position.coords.latitude);
+//       console.log(position.coords.longitude);
+//     });
+//   } else {
+//     console.log("error");
+//   }
+//   //return currentStation;
+// }
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;  
+}
+
 $(document).ready(function() {
-  getBART();
+  getLocation();
+  //getBART();
+  //getCurrentStation();
 });
